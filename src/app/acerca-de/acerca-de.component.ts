6aa9f22c059030546bd5efbcece9ffad81534a2d
lichifarias7 <lichifarias@gmail.com>
2023-11-28 20:42:26 -0300

    Commit Front  - ultimos cambios

--- src/app/acerca-de/acerca-de.component.ts
+++ src/app/acerca-de/acerca-de.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-acerca-de',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent {
-
+  
 }
+
+
